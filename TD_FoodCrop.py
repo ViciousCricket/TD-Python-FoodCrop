c156fb17ee4ab0d35086062b71de4d66a668ca59
SuperPsou <56732263+SuperPsou@users.noreply.github.com>
2019-10-20 13:09:20 +0200

    Update TD_FoodCrop.py

--- TD_FoodCrop.py
+++ TD_FoodCrop.py
@@ -8,19 +8,23 @@ class Describable(ABC):
 
 class FoodCropFactory :
     def __init__(self):
-    def createVolume() -> Unit:
-    def createPrice() -> Unit:
-    def createWeight() -> Unit:
-    def createSurface() -> Unit:
-    def createCount() -> Unit:
-    def createRatio() -> Unit:
-    def createUnitRatio() -> Unit:
-    def createCommodity() -> Commodity:
-    def createIndicator() -> Indicator:
+    def createVolume(self,ID: int) -> Unit:
+    def createPrice(self,ID: int) -> Unit:
+    def createWeight(self,ID: int,weight : float) -> Unit:
+    def createSurface(self,ID: int) -> Unit:
+    def createCount(self,ID: int,what: str) -> Unit:
+    def createRatio(self,ID: int) -> Unit:
+    def createUnitRatio(self,ID: int,unit1: Unit,unit2: Unit) -> Unit:
+    def createCommodity(self,group: CommodityGroup, ID: int, name: str) -> Commodity:
+    def createIndicator(self,ID: int frequency: int, freqDesc: str,geogLocation: str,indicatorGroup: IndicatorGroup,unit: Unit) -> Indicator:
+    def createMeasurementType(self,ID: int,description: str) -> MeasurementType:
+    def createMeasurement(self,ID: int,year: int,value: float,timeperiodId: int,timeperiodDesc: str,tipe: MeasurementType,commodity: Commodity,indicator: Indicator) -> Measurement:
     
     
 class FoodCropsDataset :
     def __init__(self):
+    def load(self,datasetPath: str):
+    def findMeasurements(self,commodityType: CommodityType = nil,indicatorGroup: IndicatorGroup = nil,geographicalLocation: str = nil,unit: Unit = nil) -> List[Measurement]:
 
 class Measurement :
     def __init__(self):
