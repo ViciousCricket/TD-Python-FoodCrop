d6a29fc476692ff059947189d5bf61b97f5fd1e3
FausseRousse <47175876+FausseRousse@users.noreply.github.com>
2019-10-23 14:31:14 +0200

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,136 +1,83 @@
+#Pour crÃ©er des classes abstraites
 from abc import ABC, abstractmethod
+
+# On dÃ©finit C comme une classe abstraite: elle hÃ©rite de ABC
 from enum import Enum
-from typing import List
 
-class Describable(ABC):
+
+class C(ABC):
+    #Le constructeur est toujours __init__(self,...) et il faut
+    #toujours faire figurer le self qui est Ã©quivalent au this de
+    #Java
     def __init__(self):
         super().__init__()
 
-class IndicatorGroup(Enum):
-    EXPORTS_AND_IMPORTS = "exports and imports"
-    SUPPLY_AND_USE = "supply and use"
-    PRICES = "prices"
-    FEED_PRICE_RATIOS = "feed price ratios"
-    QUANTITIES_FED = "quantities fed"
-    TRANSPORTATION = "transportation"
-    ANIMAL_UNIT_INDEXES = "animal unit indexes"
-    
-class CommodityGroup(Enum):
-    CORN = "corn"
-    BARLEY = "barley"
-    OATS = "oats"
-    SORGHUM = "sorghum"
-    BYPRODUCT_FEEDS ="byproduct feeds"
-    COARSE_GRAINS = "coarse grains"
-    HAY = "hay"
-    ANIMAL_PROTEIN_FEEDS = "animal protein feeds"
-    GRAIN_PROTEIN_FEEDS = "grain protein feeds"
-    PROCESSED_FEEDS = "processed feeds"
-    ENERGY_FEEDS = "energy feeds"
-    OTHER = "other"
+    #On dÃ©finit une mÃ©thode abstraite
+    @abstractmethod
+    def methode_abstraite(self):
+        pass #Si on met du code ici on aura une erreur d'execution
 
-class Commodity(Describable) :
-    def __init__(self, id : int, name : str, group : CommodityGroup):
-        self.id = id
-        self.__name = name
-        
-class MeasurementType :
-    def __init__(self, id: int, description: str):
-        self.id=id
-        self.description=description
-    
-class Unit(ABC):
-    def __init__(self, id: int, name: str):
+class A:
+    #Constructeur de la classe A, on peut donner des indices au
+    #sujet de types des paramÃ¨tres avec param: type et fixer des
+    #valeurs par dÃ©faut avec param: type = valeur
+    def __init__(self, param1: str, param2, param3: int = 0):
+        #Attribut public
+        self.attribut1 = param1
+
+        #Attributs privÃ©s, toujours avec __ en prÃ©fixe
+        self.__attrbutprive1 = param2
+
+    #Une mÃ©thode privee
+    def __ma_methode_privee(self):
+        pass
+
+    #Une mÃ©thode publique
+    def methode1(self, param4: str):
+        print(self.attribut1 + param4)
+
+    #MÃ©thode qui retourne une valeur, on peut aussi indiquer le type
+    #de retour
+    def methode2(self, valeur: int) -> int:
+        return valeur * 2
+
+#HÃ©ritage multiple
+class B(A, C):
+    def __init__(self, param1):
+        super().__init__(param1, "toto")
+
+    #Surcharge de la mÃ©thode
+    def methode1(self, param4):
+        print(param4 + self.attribut1)
+
+    def methode_abstraite(self):
+        print("Ping")
+
+class D(C):
+    def __init__(self):
         super().__init__()
-        self.name = name
-        self.id = id
 
-class Indicator(Describable) :
-    def __init__(self, id :str, frequency : int, frequencyDesc : str, geogLocation : str, indicatorGroup : IndicatorGroup, unit : Unit):
-        self.id = id
-        self.__frequency = frequency
-        self.__frequencyDesc = frequencyDesc
-        self.__geogLocation = geogLocation
+    def methode_abstraite(self):
+        print("Pong")
 
-class Measurement :
-    def __init__(self, id: int, year: int, value: float, timeperiodld: int, timeperiodDesc: str, type: MeasurementType, commodity: Commodity, indicator: Indicator):
-        self.__year=year
-        self.__value=value
-        self.__timeperiodld=timeperiodld
-        self.__timeperiodDesc=timeperiodDesc
-
-
-
-
-
-        
-
-    
-class FoodCropsDataset :
-    def __init__(self):
-        pass
-    def load(self,datasetPath: str):
-        pass
-    def findMeasurements(self, commodityGroup:CommodityGroup = None, indicatorGroup:IndicatorGroup = None, geographicalLocation:str = None, unit:Unit = None) -> List[Measurement]:
+    @staticmethod
+    def ma_methode_statique(param1):
         pass
 
 
-class Volume(Unit):
-    def __init__(self, id:int, name:str = "Volume"):
-        super().__init__(id, "Volume")
-        
-class Surface(Unit):
-    def __init__(self, id:int, name:str = "Surface"):
-        super().__init__(id, "Surface")
-        self.__name = name
+# Creer une Ã©numÃ©ration
+class MyEnum(Enum):
+    CONSTANTE_1 = 1
+    CONSTANTE_2 = 2
+    CONSTANTE_3 = 3
 
-class Price(Unit):
-    def __init__(self, id:int, name:str):
-        super().__init__(id,name)
 
-class Weight(Unit):
-    def __init__(self, id:int, name:str, multiplier:float):
-        super().__init__(id,"Weight")
-        self.__multiplier = multiplier
-            
-class Count(Unit):
-    def __init__(self, id:int, name:str, what:str):
-        super().__init__(id,"Count")
-        self.__what = what
-            
-class Ratio(Unit):
-    def __init__(self, id:int, name:str):
-        super().__init__(id,"Ratio")
-            
-class UnitRatio(Ratio):
-    def __init__(self, id:int, unit1:Unit, unit2:Unit):
-        super().__init__(id)
+# C'est quoi le nil? -> En java c'est null et
+# en python c'est None:
+#Ici paramÃ¨tre avec None comme valeur par dÃ©faut
+def methode(param1 =None):
 
-    
+    #Variable initialisÃ©e Ã  null
+    ma_variable = None
 
-    
-class FoodCropFactory :
-    def __init__(self):
-        pass
-    def createVolume(self,ID: int) -> Unit:
-        pass
-    def createPrice(self,ID: int) -> Unit:
-        pass
-    def createWeight(self,ID: int,weight : float) -> Unit:
-        pass
-    def createSurface(self,ID: int) -> Unit:
-        pass
-    def createCount(self,ID: int,what: str) -> Unit:
-        pass
-    def createRatio(self,ID: int) -> Unit:
-        pass
-    def createUnitRatio(self,ID: int,unit1: Unit,unit2: Unit) -> Unit:
-        pass
-    def createCommodity(self,group: CommodityGroup, ID: int, name: str) -> Commodity:
-        pass
-    def createIndicator(self,ID: int, frequency:int, freqDesc: str,geogLocation: str,indicatorGroup: IndicatorGroup,unit: Unit) -> Indicator:
-        pass
-    def createMeasurementType(self,ID: int,description: str) -> MeasurementType:
-        pass
-    def createMeasurement(self,ID: int,year: int,value: float,timeperiodId: int,timeperiodDesc: str,tipe: MeasurementType,commodity: Commodity,indicator: Indicator) -> Measurement:
-        pass
+
